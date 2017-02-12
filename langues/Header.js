397432ca526192dfcf652929bc325345f4e122a8
Lucas Sifoni <lucas@lucassifoni.info>
2017-02-12 19:00:05 +0100

    I18n'd header

---
+++ langues/Header.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-trailing-spaces, quotes*/
+
+export default {
+  fr: {
+    description: `est un observatoire collaboratif de l'état visuel de nos rues.`,
+    more: `chicken soup sriracha bo bûn`,
+  },
+  en: {
+    description: `is a collaborative observatory about our streets visual state`,
+    more: `ramen noodles`,
+  },
+  es: {
+
+  },
+  de: {
+
+  },
+};
