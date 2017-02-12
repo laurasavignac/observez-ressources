2580ed034ee92122e50fb8bb2cb8ec1addfd11e9
laurasavignac <laura.savignac@hotmail.fr>
2017-02-12 18:05:45 +0100

    title changes in en

--- langues/Menu.js
+++ langues/Menu.js
@@ -11,7 +11,7 @@ export default {
     contact: 'Contact',
   },
   en: {
-    title: 'Watchtower',
+    title: 'Observatory',
     home: 'Home',
     portal: 'Theme portals',
     random: 'Random article',
