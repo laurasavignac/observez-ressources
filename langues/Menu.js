857f05ab2fe50c2ba9ae95b6f599af3c023d26db
Lucas Sifoni <lucas@lucassifoni.info>
2017-02-12 18:04:11 +0100

    Added language folder & menu example

---
+++ langues/Menu.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-trailing-spaces */
+
+export default {
+  fr: {
+    title: 'Observatoire',
+    home: 'Accueil',
+    portal: 'Portail thématique',
+    random: 'Article au hasard',
+    me: 'Mon espace',
+    about: 'À propos',
+    contact: 'Contact',
+  },
+  en: {
+    title: 'Watchtower',
+    home: 'Home',
+    portal: 'Theme portals',
+    random: 'Random article',
+    me: 'My account',
+    about: 'About',
+    contact: 'Contact',
+  },
+  es: {
+
+  },
+  de: {
+
+  },
+};
