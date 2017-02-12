63549eedfa18a45aadfd023bfc666dcf1b80177f
Lucas Sifoni <lucas@lucassifoni.info>
2017-02-12 19:04:02 +0100

    I18n'd intro

---
+++ langues/Intro.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-trailing-spaces, quotes*/
+
+export default {
+  fr: {
+    sticker: `J'ai vu un autocollant :`,
+    code: `j'écris son code ici`,
+  },
+  en: {
+    sticker: `I've encountered a f**** sticker :`,
+    code: `type its code into this field`,
+  },
+  es: {
+
+  },
+  de: {
+
+  },
+};
