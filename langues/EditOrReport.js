c55eaa2ce624835a31a68774e21b089d4318745c
Lucas Sifoni <lucas@lucassifoni.info>
2017-02-17 12:34:32 +0100

    Added contribuer/signaler button

---
+++ langues/EditOrReport.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-trailing-spaces, quotes*/
+
+export default {
+  fr: {
+    contribute: `contribuer à cet article`,
+    report: `signaler`,
+  },
+  en: {
+    contribute: `edit this article`,
+    report: `report`,
+  },
+  es: {
+
+  },
+  de: {
+
+  },
+};
