e566033d3f30e0e6142898043545598a74e9a4bc
Lucas Sifoni <lucas@lucassifoni.info>
2017-02-17 12:34:56 +0100

    Section à propos

---
+++ langues/About.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-trailing-spaces, quotes*/
+import m from 'markdown';
+
+const strings = {
+  fr: {
+    title: `À propos`,
+    titleProject: `Le projet`,
+    projectContentMD: `
+### Démarche
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    titleStickers: `Les autocollants`,
+    stickersContentMD: `
+### Principe
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    titleContrib: `La contribution`,
+    contribContentMD: `
+### Comment participer ?
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  },
+  en: {
+    title: `About`,
+    titleProject: `The project`,
+    projectContentMD: `
+### Démarche
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    titleStickers: `The stickers`,
+    stickersContentMD: `
+### Principe
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    titleContrib: `Contributing`,
+    contribContentMD: `
+### Comment participer ?
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  },
+  es: {
+
+  },
+  de: {
+
+  },
+};
+/* eslint-disable no-restricted-syntax */
+for (const lang in strings) {
+  if ({}.hasOwnProperty.call(strings, lang)) {
+    for (const string in strings[lang]) {
+      if ({}.hasOwnProperty.call(strings, lang)) {
+        if (string.indexOf('MD') !== -1) {
+          strings[lang][string] = m.markdown.toHTML(strings[lang][string]);
+        }
+      }
+    }
+  }
+}
+
+export default strings;
