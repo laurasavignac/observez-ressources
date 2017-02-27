4c3c7d9cc2e5356947be402cf0bdeb43b0858e65
Lucas Sifoni <lucas@lucassifoni.info>
2017-02-27 16:13:22 +0100

    Added Contact & Contribute

---
+++ langues/Pages/Contact.js
@@ -0,0 +1,16 @@
+/**
+ * Created by lucas on 27/02/17.
+ */
+export default {
+  fr: {
+    truc: 'lalalalala',
+    trucavecapostrophesdedans: `lalalalala'lalalal'lalala`,
+    trucsurplusieurslignes: `alalalalal 
+aalalaldjkfsdjhfsd
+fdfljkdgfljkgdfljkgdfljk`,
+    trucnormal: 'normaaaaaaaaaaal',
+  },
+  en: {
+
+  },
+};
---
+++ langues/Util/Contribute.js
@@ -0,0 +1,21 @@
+/**
+ * Created by lucas on 27/02/17.
+ */
+export default {
+  fr: {
+    report: 'Signaler',
+    registerToReport: 'Connectez-vous pour signaler cette fiche',
+    addContent: 'Compléter cette fiche',
+    registerToAddContent: 'Connectez-vous pour ajouter du contenu à cette fiche',
+    editContent: 'Modifier cette fiche',
+    registerToEditContent: 'Connectez-vous pour modifier cette fiche',
+  },
+  en: {
+    report: 'report',
+    registerToReport: 'Log in to report this entry',
+    addContent: 'Complete this entry',
+    registerToAddContent: 'Log in to complete this entry',
+    editContent: 'Edit this entry',
+    registerToEditContent: 'Log in to edit this entry',
+  },
+};
