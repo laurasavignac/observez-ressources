85a8a746381788a4810587b05db952991000badb
Lucas Sifoni <lucas@lucassifoni.info>
2017-02-27 16:15:04 +0100

    Added missing backtick

--- langues/Pages/Chantier.js
+++ langues/Pages/Chantier.js
@@ -3,7 +3,7 @@ export default {
   fr: {
     title: `Laboratoire`,
     card: `Laboratoire en chantier !`,
-    intro: `Le Laboratoire, seconde partie du site, permettra de manipuler la matière récoltée afin de repérer les questionnements qui s’en dégagent, d’approfondir leurs études et d’entrevoir des perspectives d’évolution. À terme, cette partie pourra donner lieu à des publications.<br>Cette partie sera active lorsqu’il y aura assez de contenu sur le site. <br>À très vite!,
+    intro: `Le Laboratoire, seconde partie du site, permettra de manipuler la matière récoltée afin de repérer les questionnements qui s’en dégagent, d’approfondir leurs études et d’entrevoir des perspectives d’évolution. À terme, cette partie pourra donner lieu à des publications.<br>Cette partie sera active lorsqu’il y aura assez de contenu sur le site. <br>À très vite!`,
   },
   en: {
     title: `Laboratory`,
